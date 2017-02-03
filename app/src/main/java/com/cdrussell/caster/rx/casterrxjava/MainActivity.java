d5a93837f484e2886514673ef548c71aefbc87a6
Craig Russell <craig.russell.1986@gmail.com>
2017-02-03 08:46:49 +0000

    Add fromCallable as example to compare with defer

--- app/src/main/java/com/cdrussell/caster/rx/casterrxjava/MainActivity.java
+++ app/src/main/java/com/cdrussell/caster/rx/casterrxjava/MainActivity.java
@@ -9,15 +9,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func0;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button naiveButton;
-    private Button cleverButton;
+    private Button deferButton;
+    private Button fromCallableButton;
     private TextView resultTextView;
     private ProgressBar progressBar;
 
@@ -33,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private void bindViews() {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         naiveButton = (Button) findViewById(R.id.readValueButton);
-        cleverButton = (Button) findViewById(R.id.readValueDefer);
+        deferButton = (Button) findViewById(R.id.readValueDefer);
+        fromCallableButton = (Button) findViewById(R.id.readValueFromCallable);
         resultTextView = (TextView) findViewById(R.id.result);
 
         naiveButton.setOnClickListener(new View.OnClickListener() {
@@ -50,24 +55,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        cleverButton.setOnClickListener(new View.OnClickListener() {
+        deferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showProgress();
 
-                Observable<String> deferred = Observable.defer(() -> {
-                    try {
-                        return Observable.just(Database.readValue());
-                    } catch (IOException e) {
-                        return Observable.error(e);
+                Observable<String> deferred = Observable.defer(new Func0<Observable<String>>() {
+                    @Override
+                    public Observable<String> call() {
+                        try {
+                            return Observable.just(Database.readValue());
+                        } catch (IOException e) {
+                            return Observable.error(e);
+                        }
                     }
                 });
 
-                deferred.subscribeOn(Schedulers.io())
+                deferred
+                        .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(value -> {
-                            resultTextView.setText(value);
-                            hideProgress();
+                        .subscribe(new Action1<String>() {
+                            @Override
+                            public void call(String value) {
+                                resultTextView.setText(value);
+                                hideProgress();
+                            }
+                        });
+            }
+        });
+
+        fromCallableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProgress();
+
+                Observable<String> fromCallable = Observable.fromCallable(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        return Database.readValue();
+                    }
+                });
+
+                fromCallable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Action1<String>() {
+                            @Override
+                            public void call(String value) {
+                                resultTextView.setText(value);
+                                hideProgress();
+                            }
                         });
             }
         });
